008cfd9f830cdf40c71a25869a11fce211be1bfc
Tadeu Manoel <e.tadeu@gmail.com>
2019-03-01 09:57:22 -0300

    Fix to pass CMAKE_GENERATOR because CMake's default is 32-bit

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake -LAH ^
+cmake -LAH -G"%CMAKE_GENERATOR%" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     .. || goto :eof
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,10 @@ cd build
 
 CMAKE_CONFIG="Release"
 
-cmake -LAH \
+cmake -LAH -G"$CMAKE_GENERATOR" \
   -DCMAKE_BUILD_TYPE=$CMAKE_CONFIG \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   ..
 
 cmake --build . --config $CMAKE_CONFIG --target install
-
